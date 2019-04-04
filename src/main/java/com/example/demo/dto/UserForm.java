f9c510d9c1ae1dda40a9dcb6ab4e3df0b93c1125
smuraga <smuraga.darya@gmail.com>
2019-04-05 00:35:24 +0300

    Created UserForm for user registration

---
+++ src/main/java/com/example/demo/dto/UserForm.java
@@ -0,0 +1,21 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
+
+import javax.validation.constraints.Null;
+
+@Getter
+@Setter
+public class UserForm {
+    @Null
+    private Long id;
+    @NonNull
+    private String username;
+    @NonNull
+    private String firstPassword;
+    @NonNull
+    private String secondPassword;
+
+}
