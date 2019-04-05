666e14ef36b4840ac8fb8f4a38a1b90a8ec72092
smuraga <smuraga.darya@gmail.com>
2019-04-05 22:12:34 +0300

    changed findByUsername to findByLogin

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
-    User findByUsername(String username);
+    User findByLogin(String login);
     User findUserById(String id);
 }
